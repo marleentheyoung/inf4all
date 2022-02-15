f4b509a4d9efc8d91a2057e8881e251b0a495a4b
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 12:20:51 +0100

    new

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -1,9 +1,11 @@
+from sys import stdin
 import check50
 
 import pandas as pd
 import numpy as np
 import os
 import re
+import uuid
 
 # @check50.check()
 # def exists():
@@ -13,7 +15,8 @@ import re
 @check50.check()
 def check_path():
     check50.run("python3 degrees.py small")
-    
+    stdin("Emma Watson").stdout()
+    stdin("Kevin Bacon").stdout()
     # code = check50.run("./hello").exit()
     # if code != 0:
     #     raise check50.Failure(f"expected exit code 0, not {code}")
