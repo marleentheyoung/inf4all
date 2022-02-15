37962a2341bd02f925963a5af94cf7da88ec1676
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 11:27:59 +0100

    new checks

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -1,61 +1,57 @@
 import check50
+
 import pandas as pd
 import numpy as np
 import os
 import re
-import networkx as nx
 
+def spawn_small():
+    return check50.spawn("python3 adventure.py Tiny")
 
-class Degrees(Checks):
-    def spawn_tiny(self):
-        return self.spawn("python3 adventure.py Tiny")
+def spawn_large():
+    return check50.spawn("python3 adventure.py Small")
 
-    def spawn_small(self):
-        return self.spawn("python3 adventure.py Small")
+def spawn_crowther():
+    return check50.spawn("python3 adventure.py Crowther")
 
-    def spawn_crowther(self):
-        return self.spawn("python3 adventure.py Crowther")
+@check50.check()
+def exists(self):
+    """Checking if all files exist."""
+    self.require("degrees.py", "util.py")
 
-    @check()
-    def exists(self):
-        """Checking if all files exist."""
-        self.require("adventure.py", "room.py")
+    cwd = os.getcwd()
+    dest = os.path.join(cwd, "data")
+    if os.path.exists(dest):
+        os.rename(dest, dest + str(uuid.uuid4()))
+    os.mkdir(dest)
+    with check50.cd(check50.config.check_dir):
+        data_files = [os.path.join("data", data_file) for data_file in os.listdir("data")]
+        for path in data_files:
+            check50.copy(path, dest)
 
-        # self.add can't create directories :(
-        # give v3 pls
-        cwd = os.getcwd()
-        dest = os.path.join(cwd, "data")
-        if os.path.exists(dest):
-            os.rename(dest, dest + str(uuid.uuid4()))
-        os.mkdir(dest)
-        with check50.cd(check50.config.check_dir):
-            data_files = [os.path.join("data", data_file) for data_file in os.listdir("data")]
-            for path in data_files:
-                check50.copy(path, dest)
+@check50.check("exists")
+def handles_argument(self):
+    """Starting adventure with command line argument 'Tiny'."""
+    try:
+        self.spawn_tiny().stdout(re.escape(room_1_description), str_output=room_1_description)
+    except Error as error:
+        raise Error(rationale=f"Expected the description of initial "
+                                f"room when Adventure starts.\n    {error}")
 
-    @check("exists")
-    def handles_argument(self):
-        """Starting adventure with command line argument 'Tiny'."""
-        try:
-            self.spawn_tiny().stdout(re.escape(room_1_description), str_output=room_1_description)
-        except Error as error:
-            raise Error(rationale=f"Expected the description of initial "
-                                  f"room when Adventure starts.\n    {error}")
+@check50.check("handles_argument")
+def rejects_incorrect_arguments(self):
+    """Rejects incorrect amount of arguments or wrong filename."""
+    try:
+        self.spawn("python3 adventure.py").exit(1)
+    except Error as error:
+        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
 
-    @check("handles_argument")
-    def rejects_incorrect_arguments(self):
-        """Rejects incorrect amount of arguments or wrong filename."""
-        try:
-            self.spawn("python3 adventure.py").exit(1)
-        except Error as error:
-            raise Error(rationale=f"Expected an exit code of 1. Not {error}")
+    try:
+        self.spawn("python3 adventure.py Tiny CS50").exit(1)
+    except Error as error:
+        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
 
-        try:
-            self.spawn("python3 adventure.py Tiny CS50").exit(1)
-        except Error as error:
-            raise Error(rationale=f"Expected an exit code of 1. Not {error}")
-
-        try:
-            self.spawn("python3 adventure.py CS50").exit(1)
-        except Error as error:
-            raise Error(rationale=f"Expected an exit code of 1. Not {error}")
+    try:
+        self.spawn("python3 adventure.py CS50").exit(1)
+    except Error as error:
+        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
