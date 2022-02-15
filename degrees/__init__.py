dde6bf30b849dc4f77b3c3e2ab696faf9e3e1f39
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 11:56:20 +0100

    new

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -5,26 +5,19 @@ import numpy as np
 import os
 import re
 
-def spawn_small():
-    return check50.spawn("python3 adventure.py Tiny")
+class Degrees(Checks):
+    def spawn_small(self):
+        return self.check50.spawn("python3 degrees.py small")
 
-def spawn_large():
-    return check50.spawn("python3 adventure.py Small")
+    def spawn_large():
+        return self.check50.spawn("python3 degrees.py large")
 
-def spawn_crowther():
-    return check50.spawn("python3 adventure.py Crowther")
+    @check50.check()
+    def exists(self):
+        """Checking if all files exist."""
+        self.exists("degrees.py")
 
-@check50.check()
-def exists(self):
-    """Checking if all files exist."""
-    self.require("degrees.py", "util.py")
+    @check50.check()
+    def check_path(self):
+        raise NotImplementedError
 
-    cwd = os.getcwd()
-    dest = os.path.join(cwd, "data")
-    if os.path.exists(dest):
-        os.rename(dest, dest + str(uuid.uuid4()))
-    os.mkdir(dest)
-    with check50.cd(check50.config.check_dir):
-        data_files = [os.path.join("data", data_file) for data_file in os.listdir("data")]
-        for path in data_files:
-            check50.copy(path, dest)
