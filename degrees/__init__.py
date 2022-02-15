b72dce4fc9da11cc2865dcb7feb3450dcdd1141e
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 12:06:02 +0100

    new

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -5,21 +5,20 @@ import numpy as np
 import os
 import re
 
-class Degrees():
-    def spawn_small(self):
-        return self.check50.spawn("python3 degrees.py small")
 
-    def spawn_large():
-        return self.check50.spawn("python3 degrees.py large")
+def spawn_small(self):
+    return check50.spawn("python3 degrees.py small")
 
-    @check50.check()
-    def exists(self):
-        """Checking if all files exist."""
-        self.exists("degrees.py")
+def spawn_large():
+    return check50.spawn("python3 degrees.py large")
 
-    @check50.check()
-    def check_path(self):
-        check = self.spawn_small()
-        check.stdin("Emma ")
-        raise NotImplementedError
+@check50.check()
+def exists():
+    """Checking if all files exist."""
+    exists("degrees.py")
 
+@check50.check()
+def check_path():
+    check = spawn_small()
+    check.stdin("Emma Watson")
+    check.stdin("Kevin Bacon").stdout("Not connected.")
