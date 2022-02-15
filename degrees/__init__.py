72038d553d8965af024937ed739167318af0f7f5
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 12:12:29 +0100

    joe

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -5,13 +5,6 @@ import numpy as np
 import os
 import re
 
-
-def spawn_small():
-    return check50.spawn("python3 degrees.py small")
-
-def spawn_large():
-    return check50.spawn("python3 degrees.py large")
-
 # @check50.check()
 # def exists():
 #     """Checking if all files exist."""
@@ -19,6 +12,8 @@ def spawn_large():
 
 @check50.check()
 def check_path():
-    check = spawn_small()
-    check.stdin("Emma Watson")
-    check.stdin("Kevin Bacon").stdout("Not connected.")
+    check50.run("python3 degrees.py small")
+
+    # code = check50.run("./hello").exit()
+    # if code != 0:
+    #     raise check50.Failure(f"expected exit code 0, not {code}")
