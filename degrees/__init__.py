ff3ef01d89c6cc1a5732d2664a82e6a531c81ce1
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-15 11:29:57 +0100

    init

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -28,30 +28,3 @@ def exists(self):
         data_files = [os.path.join("data", data_file) for data_file in os.listdir("data")]
         for path in data_files:
             check50.copy(path, dest)
-
-@check50.check("exists")
-def handles_argument(self):
-    """Starting adventure with command line argument 'Tiny'."""
-    try:
-        self.spawn_tiny().stdout(re.escape(room_1_description), str_output=room_1_description)
-    except Error as error:
-        raise Error(rationale=f"Expected the description of initial "
-                                f"room when Adventure starts.\n    {error}")
-
-@check50.check("handles_argument")
-def rejects_incorrect_arguments(self):
-    """Rejects incorrect amount of arguments or wrong filename."""
-    try:
-        self.spawn("python3 adventure.py").exit(1)
-    except Error as error:
-        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
-
-    try:
-        self.spawn("python3 adventure.py Tiny CS50").exit(1)
-    except Error as error:
-        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
-
-    try:
-        self.spawn("python3 adventure.py CS50").exit(1)
-    except Error as error:
-        raise Error(rationale=f"Expected an exit code of 1. Not {error}")
