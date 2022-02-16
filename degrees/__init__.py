dff994df6eae10fd48d5db3e65da2a8323804200
marleentheyoung <marleen.dejonge@outlook.com>
2022-02-16 15:41:46 +0100

    prompt check

--- degrees/__init__.py
+++ degrees/__init__.py
@@ -15,10 +15,11 @@ import uuid
 @check50.check()
 def check_path():
     """ Correctly finds path in small dataset """
-    check = check50.run("python3 degrees.py small").stdin("Emma Watson", prompt=True).stdin("Kevin Bacon", prompt=True)
+    check = check50.run("python3 degrees.py small").stdin("Emma Watson", prompt=True)
+    # .stdin("Kevin Bacon", prompt=True)
     
     # Check if path has been found
-    check.stdout(re.escape("Not connected."))
+    # check.stdout(re.escape("Not connected."))
 
     # code = check50.run("./hello").exit()
     # if code != 0:
